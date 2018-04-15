5d6f87bc0179811cd0f78b67fb2d413302047e52
dagaaa <dagmaragoj@gmail.com>
2018-04-15 12:34:31 +0200

    Initial commit

---
+++ src/Doctor.java
@@ -0,0 +1,14 @@
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Doctor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
